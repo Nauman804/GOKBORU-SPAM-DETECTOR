6ffaef459fa333d2970d77d70c41f986c07812b0
noman7918 <maliknomandhudhimithu@gmail.com>
2025-11-17 14:25:24 +0500

    fix

--- app/main.py
+++ app/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.responses import FileResponse
+from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 import joblib
 
@@ -15,17 +15,20 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-# load ML model
+# Serve static files (MUST be above routes)
+app.mount("/static", StaticFiles(directory="app/static"), name="static")
+
+# Load model
 model = joblib.load("app/models/spam_model.pkl")
 tfidf = joblib.load("app/models/vectorizer.pkl")
-
 
 class Message(BaseModel):
     text: str
 
-@app.get("/")
+@app.get("/", response_class=HTMLResponse)
 def home():
-    return FileResponse("app/static/index.html")
+    with open("app/static/index.html", "r", encoding="utf-8") as f:
+        return f.read()
 
 @app.post("/predict")
 def predict_text(data: Message):
@@ -33,6 +36,3 @@ def predict_text(data: Message):
     prediction = model.predict(vector)[0]
     label = "spam" if prediction == 1 else "ham"
     return {"text": data.text, "prediction": label}
-
-# serve CSS, JS, images if any
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
