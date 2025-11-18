38e31c7c4bbf1a9e192f0dbba7d23610b0443b36
noman7918 <maliknomandhudhimithu@gmail.com>
2025-11-18 08:56:05 +0500

    FIX

--- app/main.py
+++ app/main.py
@@ -1,8 +1,8 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
+from fastapi.middleware.cors import CORSMiddleware
 import joblib
 import os
 
@@ -16,6 +16,16 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
+# Path where index.html is stored
+STATIC_DIR = os.path.join(os.path.dirname(__file__), "static")
+
+app.mount("/static", StaticFiles(directory=STATIC_DIR), name="static")
+
+# Serve homepage
+@app.get("/")
+def home():
+    return FileResponse(os.path.join(STATIC_DIR, "index.html"))
+
 # Load model
 model = joblib.load("app/models/spam_model.pkl")
 tfidf = joblib.load("app/models/vectorizer.pkl")
@@ -23,18 +33,11 @@ tfidf = joblib.load("app/models/vectorizer.pkl")
 class Message(BaseModel):
     text: str
 
-# Serve homepage
-@app.get("/")
-def home():
-    return FileResponse("app/static/index.html")
-
-# Serve static files
-app.mount("/static", StaticFiles(directory="app/static"), name="static")
-
 @app.post("/predict")
 def predict_text(data: Message):
     vector = tfidf.transform([data.text])
     prediction = model.predict(vector)[0]
     label = "spam" if prediction == 1 else "ham"
     return {"text": data.text, "prediction": label}
+
 
